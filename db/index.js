23d4d4e8757ac6e9f0fe6ccaee29ac43647a2fe8
fliszt2 <rkelly2012@gmail.com>
2021-03-18 14:34:56 -0400

    Querying is fast now with btree indices

--- db/index.js
+++ db/index.js
@@ -3,22 +3,35 @@ const { Client } = require('pg');
 const client = new Client({ database: 'mydb' });
 
 client.fetchReviews = ({ product_id, count = 5, page = 1 }, cb) => {
-  // console.log('product_id, count, page from client:', product_id, count, page);
-  // query client for reviews with this product_id
   client.query(`SELECT * FROM reviews WHERE product_id = ${product_id};`, (err, res) => {
     if (err) { return cb(err); };
     var reviews = res.rows;
+    reviews.forEach(review => {
+      review.photos = [];
+      review.review_id = review.id;
+      delete review.id;
+      delete review.product_id;
+    });
     console.log('reviews:', reviews);
     if (reviews.length > 0) {
-      var review_ids = reviews.map(review => review.id.toString());
+      var review_ids = reviews.map(review => review.review_id.toString());
       client.query(`SELECT * FROM reviews_photos WHERE review_id IN (${review_ids.join(',')});`, (err, res) => {
         if (err) { return cb(err); };
         var photos = res.rows;
+        photos.forEach(photo => {
+          var { review_id } = photo;
+          delete photo.review_id;
+          reviews.forEach(review => {
+            if (review.review_id === review_id) {
+              review.photos.push(photo);
+            }
+          })
+        });
         console.log('photos:', photos);
-        cb(null, 'temp');
+        cb(null, reviews);
       });
     } else {
-      cb(null, 'no photos');
+      cb(null, reviews);
     }
   });
 };
